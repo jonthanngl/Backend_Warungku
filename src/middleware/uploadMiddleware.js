85602c4fc5ed49b89acfc83e36954615f28c7d40
jonthanngl <jonathannenggolan@gmail.com>
2025-12-16 23:01:37 +0700

    Update uploadMiddleware.js

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -1,16 +1,10 @@
 const multer = require('multer');
-const path = require('path');
+// Kita import 'storage' yang sudah dibuat di file config cloudinary
+const { storage } = require('../config/cloudinary'); 
 
-// Setup Multer (Upload Gambar)
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, 'public/uploads/'); 
-  },
-  filename: (req, file, cb) => {
-    cb(null, Date.now() + path.extname(file.originalname));
-  }
+const upload = multer({ 
+  storage: storage,
+  limits: { fileSize: 5 * 1024 * 1024 } // Opsional: Batas ukuran file (contoh: 5MB)
 });
-
-const upload = multer({ storage: storage });
 
 module.exports = upload;
