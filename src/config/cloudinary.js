9299002c4ec0f2e18784c21ded074decab5cc208
fadlyhudayaaja <fadlyhudaya@students.usu.ac.id>
2025-12-16 23:00:12 +0700

    Create cloudinary.js

---
+++ src/config/cloudinary.js
@@ -0,0 +1,19 @@
+const cloudinary = require('cloudinary').v2;
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+
+// Mengambil data dari file .env
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'warung-fix-menu',
+    allowed_formats: ['jpg', 'png', 'jpeg', 'webp'],
+  },
+});
+
+module.exports = { cloudinary, storage };
