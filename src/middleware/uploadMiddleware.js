9bdb582f919007ae52645067d2f04406a217252f
fadlyhudayaaja <fadlyhudaya@students.usu.ac.id>
2025-12-17 20:52:59 +0700

    Update uploadMiddleware.js

--- src/middleware/uploadMiddleware.js
+++ src/middleware/uploadMiddleware.js
@@ -1,10 +1,9 @@
 const multer = require('multer');
-// Kita import 'storage' yang sudah dibuat di file config cloudinary
 const { storage } = require('../config/cloudinary'); 
 
 const upload = multer({ 
   storage: storage,
-  limits: { fileSize: 5 * 1024 * 1024 } // Opsional: Batas ukuran file (contoh: 5MB)
+  limits: { fileSize: 5 * 1024 * 1024 }
 });
 
 module.exports = upload;
