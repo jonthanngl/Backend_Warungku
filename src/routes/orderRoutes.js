dfb27f9fa9af22929e37f54e5287bd93fd6282a4
jonthanngl <jonathannenggolan@gmail.com>
2025-12-24 19:49:26 +0700

    Update orderRoutes.js

--- src/routes/orderRoutes.js
+++ src/routes/orderRoutes.js
@@ -3,14 +3,19 @@ const router = express.Router();
 const orderController = require('../controllers/orderController');
 const { protect, adminOnly } = require('../middleware/authMiddleware'); 
 
-// USER ROUTES
-router.post('/', orderController.createOrder);
-router.get('/:transaction_code', orderController.getOrderStatus);
+// 1. Buat pesanan (User Login)
+router.post('/', protect, orderController.createOrder);
 
-// USER ROUTE DENGAN PROTEKSI (History telah dihapus)
+// 2. Ambil riwayat pesanan sendiri (User Login) <-- INI WAJIB ADA
+router.get('/history', protect, orderController.getUserOrders);
 
-// ADMIN ROUTES (DI PROTEKSI)
-router.get('/', protect, adminOnly, orderController.getAllOrders);     
-router.put('/:id', protect, adminOnly, orderController.updateOrderStatus); 
+// 3. Track pesanan (Umum/Tanpa Login)
+router.get('/track/:transaction_code', orderController.getOrderStatus);
+
+// 4. Ambil SEMUA pesanan (KHUSUS ADMIN)
+router.get('/', protect, adminOnly, orderController.getAllOrders);
+
+// 5. Update Status Pesanan (KHUSUS ADMIN)
+router.put('/:id', protect, adminOnly, orderController.updateOrderStatus);
 
 module.exports = router;
