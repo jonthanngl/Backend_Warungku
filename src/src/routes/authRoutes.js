49eeb1d7136911435e065447bc73b8c029a0dd14
jonthanngl <jonathannenggolan@gmail.com>
2025-12-24 19:34:51 +0700

    Delete src/src directory

--- src/src/routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const authController = require('../controllers/authController');
-
-// POST /api/auth/login
-router.post('/login', authController.login);
-router.post('/register', authController.register);
-
-module.exports = router;
--- src/src/routes/dashboardRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const dashboardController = require('../controllers/dashboardController');
-const { protect, adminOnly } = require('../middleware/authMiddleware'); 
-
-// Rute untuk mengambil ringkasan penjualan
-router.get('/sales', protect, adminOnly, dashboardController.getSalesSummary);
-
-module.exports = router;
